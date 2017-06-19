281cb60c74220fcddcccf2b7ff75caf351597f4f
Liz Rice <liz@lizrice.com>
2017-06-19 17:25:35 +0100

    Replace the default help text

--- cmd/root.go
+++ cmd/root.go
@@ -41,13 +41,8 @@ var (
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "cis_kubernetes",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Run CIS Benchmarks checks against a Kubernetes deployment",
+	Long:  `This tool runs the CIS Kubernetes 1.6 Benchmark v1.0.0 checks.`,
 }
 
 // Execute adds all child commands to the root command sets flags appropriately.
