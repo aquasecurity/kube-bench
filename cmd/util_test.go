60982e0081b16696f922c80058cbf03c19ff62e1
Abubakr-Sadik Nii Nai Davis <dwa2pac@gmail.com>
2017-08-29 15:50:09 +0000

    Add initial support for multiple versions of kubernetes.

--- cmd/util_test.go
+++ cmd/util_test.go
@@ -20,60 +20,18 @@ import (
 	"testing"
 )
 
-func TestCheckVersion(t *testing.T) {
-	kubeoutput := `Client Version: version.Info{Major:"1", Minor:"7", GitVersion:"v1.7.0", GitCommit:"d3ada0119e776222f11ec7945e6d860061339aad", GitTreeState:"clean", BuildDate:"2017-06-30T09:51:01Z", GoVersion:"go1.8.3", Compiler:"gc", Platform:"darwin/amd64"}
-	Server Version: version.Info{Major:"1", Minor:"7", GitVersion:"v1.7.0", GitCommit:"d3ada0119e776222f11ec7945e6d860061339aad", GitTreeState:"clean", BuildDate:"2017-07-26T00:12:31Z", GoVersion:"go1.8.3", Compiler:"gc", Platform:"linux/amd64"}`
-	cases := []struct {
-		t     string
-		s     string
-		major string
-		minor string
-		exp   string
-	}{
-		{t: "Client", s: kubeoutput, major: "1", minor: "7"},
-		{t: "Server", s: kubeoutput, major: "1", minor: "7"},
-		{t: "Client", s: kubeoutput, major: "1", minor: "6", exp: "Unexpected Client version 1.7"},
-		{t: "Client", s: kubeoutput, major: "2", minor: "0", exp: "Unexpected Client version 1.7"},
-		{t: "Server", s: "something unexpected", major: "2", minor: "0", exp: "Couldn't find Server version from kubectl output 'something unexpected'"},
-	}
+func TestGetKubeVersion(t *testing.T) {
+	ver := getKubeVersion()
+	if ver == nil {
+		t.Log("Expected non nil version info.")
+	} else {
+		if ok, err := regexp.MatchString(`\d+.\d+`, ver.Client); !ok && err != nil {
+			t.Logf("Expected:%v got %v\n", "n.m", ver.Client)
+		}
 
-	for id, c := range cases {
-		t.Run(strconv.Itoa(id), func(t *testing.T) {
-			m := checkVersion(c.t, c.s, c.major, c.minor)
-			if m != c.exp {
-				t.Fatalf("Got: %s, expected: %s", m, c.exp)
-			}
-		})
-	}
-
-}
-
-func TestVersionMatch(t *testing.T) {
-	minor := regexVersionMinor
-	major := regexVersionMajor
-	client := `Client Version: version.Info{Major:"1", Minor:"7", GitVersion:"v1.7.0", GitCommit:"d3ada0119e776222f11ec7945e6d860061339aad", GitTreeState:"clean", BuildDate:"2017-06-30T09:51:01Z", GoVersion:"go1.8.3", Compiler:"gc", Platform:"darwin/amd64"}`
-	server := `Server Version: version.Info{Major:"1", Minor:"7", GitVersion:"v1.7.0", GitCommit:"d3ada0119e776222f11ec7945e6d860061339aad", GitTreeState:"clean", BuildDate:"2017-07-26T00:12:31Z", GoVersion:"go1.8.3", Compiler:"gc", Platform:"linux/amd64"}`
-
-	cases := []struct {
-		r   *regexp.Regexp
-		s   string
-		exp string
-	}{
-		{r: major, s: server, exp: "1"},
-		{r: minor, s: server, exp: "7"},
-		{r: major, s: client, exp: "1"},
-		{r: minor, s: client, exp: "7"},
-		{r: major, s: "Some unexpected string"},
-		{r: minor}, // Checking that we don't fall over if the string is empty
-	}
-
-	for id, c := range cases {
-		t.Run(strconv.Itoa(id), func(t *testing.T) {
-			m := versionMatch(c.r, c.s)
-			if m != c.exp {
-				t.Fatalf("Got %s expected %s", m, c.exp)
-			}
-		})
+		if ok, err := regexp.MatchString(`\d+.\d+`, ver.Server); !ok && err != nil {
+			t.Logf("Expected:%v got %v\n", "n.m", ver.Server)
+		}
 	}
 }
 
