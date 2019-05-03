8c8ae7ce76310abac56ba54c9242956b42fd21c2
Liz Rice <liz@lizrice.com>
2019-05-02 18:15:05 -0700

    Update copyright date

--- check/controls_test.go
+++ check/controls_test.go
@@ -1,4 +1,4 @@
-// Copyright © 2017 Aqua Security Software Ltd. <info@aquasec.com>
+// Copyright © 2017-2019 Aqua Security Software Ltd. <info@aquasec.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
