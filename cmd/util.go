fff86daf3efa0734bc547778ab1ee653692d374c
Roberto Rojas <robertojrojas@gmail.com>
2019-11-01 10:00:31 -0400

    Update cmd/util.go
    Co-Authored-By: Liz Rice <liz@lizrice.com>

--- cmd/util.go
+++ cmd/util.go
@@ -36,7 +36,7 @@ var TypeMap = map[string][]string{
 }
 
 var errMissingKubectlKubelet = fmt.Errorf(`Unable to find the programs kubectl or kubelet in the PATH.
-These programs are used to determine version of kubernetes.
+These programs are used to determine which version of Kubernetes is running.
 Make sure the /usr/bin directory is mapped to the container, 
 either using the job.yaml file used, or docker command.
 
