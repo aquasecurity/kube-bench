b52d33f9bd92c621538ba3556076563caeefc3e7
Roberto J Rojas <robertojrojas@gmail.com>
2019-11-15 14:50:08 -0500

    adds comparison

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -5,6 +5,8 @@ package integration
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"strings"
 	"testing"
 	"time"
 )
@@ -17,35 +19,51 @@ func TestRunWithKind(t *testing.T) {
 	timeout := time.Duration(10 * time.Minute)
 	ticker := time.Duration(2 * time.Second)
 
+	mustMatch := func(expFname, data string) {
+		d, err := ioutil.ReadFile(expFname)
+		if err != nil {
+			t.Error(err)
+		}
+		expectedData := strings.TrimSpace(string(d))
+		data = strings.TrimSpace(data)
+		if expectedData != data {
+			t.Errorf("expected: %q\n\n Got %q\n\n", expectedData, data)
+		}
+	}
+
 	cases := []struct {
 		TestName      string
 		KindCfg       string
 		KubebenchYAML string
+		ExpectedFile  string
 		ExpectError   bool
 	}{
 		{
 			TestName:      "job",
 			KindCfg:       "./testdata/add-tls-kind.yaml",
 			KubebenchYAML: "../job.yaml",
+			ExpectedFile:  "./testdata/job.data",
 			ExpectError:   false,
 		},
 		{
 			TestName:      "job-node",
 			KindCfg:       "./testdata/add-tls-kind.yaml",
 			KubebenchYAML: "../job-node.yaml",
+			ExpectedFile:  "./testdata/job-node.data",
 			ExpectError:   false,
 		},
 		{
 			TestName:      "job-master",
 			KindCfg:       "./testdata/add-tls-kind.yaml",
 			KubebenchYAML: "../job-master.yaml",
+			ExpectedFile:  "./testdata/job-master.data",
 			ExpectError:   false,
 		},
 	}
 	for _, c := range cases {
 		t.Run(c.TestName, func(t *testing.T) {
-			output, err := runWithKind(c.TestName, c.KindCfg, c.KubebenchYAML, *kubebenchImg, timeout, ticker)
-			fmt.Printf("CLUSTER %s \n\n %s", c.TestName, output)
+			data, err := runWithKind(c.TestName, c.KindCfg, c.KubebenchYAML, *kubebenchImg, timeout, ticker)
+			fmt.Printf("CLUSTER %s \n\n %s", c.TestName, data)
 			if err != nil {
 				if !c.ExpectError {
 					t.Fatalf("unexpected error: %v", err)
@@ -55,6 +73,8 @@ func TestRunWithKind(t *testing.T) {
 
 			if c.ExpectError {
 				t.Fatalf("unexpected lack or error while Loading config")
+			} else {
+				mustMatch(c.ExpectedFile, data)
 			}
 		})
 	}
