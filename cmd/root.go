a8269195b01d216a875041af4f2c8410103342b0
Roberto J Rojas <robertojrojas@gmail.com>
2019-12-13 10:43:33 -0500

    fixes code error

--- cmd/root.go
+++ cmd/root.go
@@ -64,11 +64,7 @@ var RootCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		benchmarkVersion, err := getBenchmarkVersion(kubeVersion, benchmarkVersion, viper.GetViper())
 		if err != nil {
-<<<<<<< HEAD
-			exitWithError(fmt.Errorf("unable to get benchmark version. error: %v", err))
-=======
 			exitWithError(fmt.Errorf("unable to determine benchmark version: %v", err))
->>>>>>> master
 		}
 
 		if isMaster() {
