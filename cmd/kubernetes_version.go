98063b772402300cca0d761f34ea870fb092a428
Roberto Rojas <robertojrojas@gmail.com>
2020-03-05 07:42:58 -0500

    Update cmd/kubernetes_version.go
    Co-Authored-By: Liz Rice <liz@lizrice.com>

--- cmd/kubernetes_version.go
+++ cmd/kubernetes_version.go
@@ -45,6 +45,7 @@ func getKubeVersionFromRESTAPI() (string, error) {
 
 func getWebDataWithRetry(k8sVersionURL, token string, cacert *tls.Certificate) (data []byte, err error) {
 	tries := 0
+      // We retry a few times in case the DNS service has not had time to come up
 	for tries < 10 {
 		data, err = getWebData(k8sVersionURL, token, cacert)
 		if err == nil {
