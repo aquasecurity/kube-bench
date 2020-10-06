0b85fd4ddedfe1dfc574eca04cbed9277c6ae279
Yoav Rotem <yoavrotems97@gmail.com>
2020-10-06 13:00:24 +0300

    Remove test relate lines
    We don't test while running, only integration testing when building and unit testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,11 @@ if [ "$1" == "install" ]; then
   if [ -d /host ]; then
     mkdir -p /host/cfg/
     yes | cp -rf cfg/* /host/cfg/
-    yes | cp -rf integration/ /host/integration/
     yes | cp -rf /usr/local/bin/kube-bench /host/
     echo "==============================================="
     echo "kube-bench is now installed on your host       "
     echo "Run ./kube-bench to perform a security check   "
     echo "==============================================="
-    cat /host/integration/testdata/cis-1.5/job-node.data
   else
     echo "Usage:"
     echo "  install: docker run --rm -v \`pwd\`:/host aquasec/kube-bench install"
