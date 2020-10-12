5bb1a3e6691ccde92ece6cf27fd6076df8fd1150
Anand prabhakar <anandprabhakar0507@gmail.com>
2020-10-13 05:17:00 +0530

    interactive code change request

--- main.go
+++ main.go
@@ -11,13 +11,16 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+import "fmt"
 package main
 
 import (
 	"github.com/aquasecurity/kube-bench/cmd"
 )
 
+
+
 func main() {
+	fmt.Println("Execution starting")
 	cmd.Execute()
 }
