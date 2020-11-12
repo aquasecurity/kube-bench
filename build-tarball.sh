c8c92d3250f8db0f91665c2519377ce2d6412cab
Noah Kraemer <noah.kraemer@sysdig.com>
2020-11-12 11:32:07 -0800

    perform zip in correct directory.

--- build-tarball.sh
+++ build-tarball.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -e
 mkdir -p out/${OUTPUT_FILENAME}
+cd out
 
-go build -o out/${OUTPUT_FILENAME}/kube-bench .
-cp LICENSE out/${OUTPUT_FILENAME}/LICENSE
-cp README.md out/${OUTPUT_FILENAME}/README.md
-cp -rf cfg out/${OUTPUT_FILENAME}/cfg
+go build -o ${OUTPUT_FILENAME}/kube-bench .
+cp LICENSE ${OUTPUT_FILENAME}/LICENSE
+cp README.md ${OUTPUT_FILENAME}/README.md
+cp -rf cfg ${OUTPUT_FILENAME}/cfg
 
-tar -czf out/${OUTPUT_FILENAME}.tar.gz out/${OUTPUT_FILENAME}
+tar -czf ${OUTPUT_FILENAME}.tar.gz ${OUTPUT_FILENAME}
