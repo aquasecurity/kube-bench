2d033edc96f526879de30a8ae483893c09d69da0
Yoav Rotem <yoavrotems97@gmail.com>
2021-06-16 20:55:04 +0300

    New cis v1.20 (#912)
    * Add files via upload
    
    * Add new cis support v1.20!
    
    * Fix issue with 1.1.9 and 1.1.10 tests
    
    Tests in some cases stat empty path which will return error.
    
    * Add tests for kubernetes 1.20 and retire 1.15 tests
    
    kubernetes 1.15 is not supported anymore and we shouldn't keep testing it.
    
    * Kubernetes 1.15 is not supported anymore
    
    * Tests for kubernetes 1.20
    
    * Fix yamllint errors
    
    Removed trailing spaces (trailing-spaces)
    
    * Add tests for v1.20
    
    * Remove extra spaces
    
    * Change cis test functions names

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -80,12 +80,12 @@ func testCheckCISWithKind(t *testing.T, testdataDir string) {
 	}
 }
 
-func TestCheckCIS15WithKind(t *testing.T) {
-	testCheckCISWithKind(t, "cis-1.5")
-}
-
 func TestCheckCIS16WithKind(t *testing.T) {
 	testCheckCISWithKind(t, "cis-1.6")
+}
+
+func TestCheckCIS120WithKind(t *testing.T) {
+	testCheckCISWithKind(t, "cis-1.20")
 }
 
 // This is simple "diff" between 2 strings containing multiple lines.
