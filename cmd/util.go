143a7bdf758416246590ee76e51e88684c0be1cc
Laiba Bareera <laibabareeradcg@gmail.com>
2025-08-01 14:02:22 +0500

    fix the issue mentioned in pr

--- cmd/util.go
+++ cmd/util.go
@@ -522,9 +522,8 @@ func getPlatformBenchmarkVersion(platform Platform) string {
 	switch platform.Name {
 	case "eks":
 		switch platform.Version {
-		case "1.29", "1.30", "1.31", "1.32", "1.33": // add more if needed
-			return "eks-1.7.0"
-		case "1.15", "1.16", "1.17", "1.18", "1.19", "1.20", "1.21", "1.22", "1.23", "1.24", "1.25", "1.26", "1.27", "1.28":
+		case "1.15", "1.16", "1.17", "1.18", "1.19", "1.20", "1.21", "1.22",
+		"1.23", "1.24", "1.25", "1.26", "1.27", "1.28":
 			return "eks-1.5.0"
 		default:
 			return "eks-1.7.0"
