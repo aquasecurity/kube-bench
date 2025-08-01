94edac3059e1cd28ca82eae53620849c0a359496
Laiba Bareera <laibabareeradcg@gmail.com>
2025-08-01 14:07:56 +0500

    fix linter error

--- cmd/util.go
+++ cmd/util.go
@@ -523,7 +523,7 @@ func getPlatformBenchmarkVersion(platform Platform) string {
 	case "eks":
 		switch platform.Version {
 		case "1.15", "1.16", "1.17", "1.18", "1.19", "1.20", "1.21", "1.22",
-		"1.23", "1.24", "1.25", "1.26", "1.27", "1.28":
+			"1.23", "1.24", "1.25", "1.26", "1.27", "1.28":
 			return "eks-1.5.0"
 		default:
 			return "eks-1.7.0"
